0637e149695701a4e005d7643293620e337bf663
Ozmar Mendoza <ozmarmen@gmail.com>
2022-09-16 23:16:27 -0500

    Create /destination page

--- pages/destination/index.jsx
+++ pages/destination/index.jsx
@@ -1,5 +1,8 @@
+import styles from "./index.module.css";
+
 import fs from "fs/promises";
 import path from "path";
+import DestinationTabList from "../../components/DestinationTabList";
 
 /**
  * @param {object} props
@@ -7,25 +10,79 @@ import path from "path";
  */
 export default function Destination({ destinationsData }) {
 	return (
-		<div>
-			<div>
-				01 <h1>Pick your destination</h1>
-			</div>
-			<ul>
-				<li>Moon</li>
-				<li>Mars</li>
-				<li>Europa</li>
-				<li>Titan</li>
-			</ul>
-			{destinationsData.map((destination, i) => (
-				<div key={i}>
-					<p>{destination.name}</p>
-					<p>{destination.description}</p>
-					<p>Avg. distance {destination.distance}</p>
-					<p>Est. travel time {destination.travel}</p>
+		<>
+			<main className={`page-section-container`}>
+				<div className={`${styles.headingWrapper}`}>
+					<span className={`${styles.headingCounter}`}>01</span>{" "}
+					<h1
+						className="text-subheading2"
+						style={{
+							display: "inline-block",
+							marginInlineStart: 0.5 + "em",
+						}}
+					>
+						Pick your destination
+					</h1>
 				</div>
-			))}
-		</div>
+				<div className={`${styles.destinationSelectorWrapper}`}>
+					<li>Moon</li>
+					<li>Mars</li>
+					<li>Europa</li>
+					<li>Titan</li>
+				</div>
+				<DestinationTabList />
+				{/* {destinationsData.map((destination, i) => (
+					<article className={`${styles.contentWrapper}`} key={i}>
+						<picture>
+							<source
+								srcSet={destination.images.webp}
+								type="image/webp"
+							/>
+							<img
+								src={destination.images.png}
+								type="image/png"
+								alt={`${destination.name}`}
+							/>
+						</picture>
+						<h2
+							className={`text-xxxl`}
+							style={{ textAlign: "center" }}
+						>
+							{destination.name}
+						</h2>
+						<p style={{ textAlign: "center" }}>
+							{destination.description}
+						</p>
+						<div style={{ textAlign: "center" }}>
+							<p>Avg. distance</p>
+							<p>{destination.distance}</p>
+						</div>
+						<div style={{ textAlign: "center" }}>
+							<p>Est. travel time</p>
+							<p>{destination.travel}</p>
+						</div>
+					</article>
+				))} */}
+			</main>
+			<picture>
+				<source
+					srcSet="/assets/destination/background-destination-desktop.jpg"
+					type="image/jpeg"
+					media="(min-width: 1024px)"
+				/>
+				<source
+					srcSet="/assets/destination/background-destination-tablet.jpg"
+					type="image/jpeg"
+					media="(min-width: 768px)"
+				/>
+				<img
+					className={`page-bg`}
+					src="/assets/destination/background-destination-mobile.jpg"
+					alt=""
+					role="presentation"
+				/>
+			</picture>
+		</>
 	);
 }
 

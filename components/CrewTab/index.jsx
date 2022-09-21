e5cdf2a2cb6bc3f044fe5b515b819692f3d16a2d
Ozmar Mendoza <ozmarmen@gmail.com>
2022-09-21 16:21:43 -0500

    Crew page mobile styles

---
+++ components/CrewTab/index.jsx
@@ -0,0 +1,33 @@
+import styles from "./index.module.css";
+
+/**
+ * @param {object} props
+ * @param {{name: string, images: {png: string, webp: string}, role: string, bio: string}[]} props.data
+ */
+export default function CrewTab({ data }) {
+	return (
+		<>
+			<div className={styles.detailsWrapper}>
+				<p
+					className={`text-xl ${styles.role}`}
+					style={{ opacity: 0.5 }}
+				>
+					{data.role}
+				</p>
+				<h2 className={`text-xxl ${styles.name}`}>{data.name}</h2>
+				<p className={styles.bio}>{data.bio}</p>
+			</div>
+			<div className={styles.photoWrapper}>
+				<picture>
+					<source srcSet={data.images.webp} type="image/webp" />
+					<img
+						className={styles.photo}
+						src={data.images.png}
+						type="image/png"
+						alt={data.name}
+					/>
+				</picture>
+			</div>
+		</>
+	);
+}
